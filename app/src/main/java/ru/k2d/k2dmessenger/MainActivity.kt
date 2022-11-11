3cea343954f43ba1205b31e5bc62615f5612a847
K2D2021 <zxtreme@yandex.ru>
2022-11-11 22:14:34 +0300

    Initial commit

---
+++ app/src/main/java/ru/k2d/k2dmessenger/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.k2d.k2dmessenger
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
