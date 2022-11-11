6c96802c7084d0f17c6ec7b93521eaf251cd0cb8
K2D2021 <zxtreme@yandex.ru>
2022-11-11 22:51:56 +0300

    Add dependencies

--- app/src/main/java/ru/k2d/k2dmessenger/MainActivity.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/MainActivity.kt
@@ -2,8 +2,11 @@ package ru.k2d.k2dmessenger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.k2d.k2dmessenger.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
