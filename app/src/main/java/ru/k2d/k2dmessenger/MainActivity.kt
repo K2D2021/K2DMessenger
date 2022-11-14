f80bcf88a9a20140205f25ec3c8f67a4bd91fdb7
K2D2021 <zxtreme@yandex.ru>
2022-11-15 00:36:22 +0300

    Add and implement SettingsFragment.kt

--- app/src/main/java/ru/k2d/k2dmessenger/MainActivity.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/MainActivity.kt
@@ -1,9 +1,8 @@
 package ru.k2d.k2dmessenger
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.mikepenz.materialdrawer.AccountHeader
 import com.mikepenz.materialdrawer.AccountHeaderBuilder
@@ -15,6 +14,7 @@ import com.mikepenz.materialdrawer.model.ProfileDrawerItem
 import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
 import ru.k2d.k2dmessenger.databinding.ActivityMainBinding
 import ru.k2d.k2dmessenger.ui.ChatsFragment
+import ru.k2d.k2dmessenger.ui.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -103,7 +103,11 @@ class MainActivity : AppCompatActivity() {
                     position: Int,
                     drawerItem: IDrawerItem<*>
                 ): Boolean {
-                    Toast.makeText(applicationContext, position.toString(), Toast.LENGTH_SHORT).show()
+                    when(position){
+                        7 -> supportFragmentManager.beginTransaction()
+                            .addToBackStack(null)
+                            .replace(R.id.dataContainer,SettingsFragment()).commit()
+                    }
                     return false
                 }
             }).build()
---
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/SettingsFragment.kt
@@ -0,0 +1,24 @@
+package ru.k2d.k2dmessenger.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.k2d.k2dmessenger.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+
+    private lateinit var mBinding: FragmentSettingsBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        mBinding = FragmentSettingsBinding.inflate(layoutInflater)
+        return mBinding.root
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+}
