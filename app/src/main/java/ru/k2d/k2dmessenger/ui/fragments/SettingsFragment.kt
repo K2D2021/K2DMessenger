adef329164858528a29f5e12a1fc6ae7428f9af3
K2D2021 <zxtreme@yandex.ru>
2022-11-18 00:59:10 +0300

    Add menu with icons for fragment_settings.xml

--- app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/SettingsFragment.kt
@@ -1,10 +1,17 @@
 package ru.k2d.k2dmessenger.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
 import ru.k2d.k2dmessenger.R
 
 class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.settings_action_menu,menu)
     }
 }
