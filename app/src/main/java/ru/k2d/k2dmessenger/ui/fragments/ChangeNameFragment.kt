f2c951d2b5bee4377b54c1900b0aec7f1df355f6
K2D2021 <zxtreme@yandex.ru>
2022-12-01 01:35:06 +0300

    Complete changeName flow.

--- app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/ChangeNameFragment.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/ChangeNameFragment.kt
@@ -1,13 +1,45 @@
 package ru.k2d.k2dmessenger.ui.fragments
 
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_change_name.*
+import ru.k2d.k2dmessenger.MainActivity
 import ru.k2d.k2dmessenger.R
+import ru.k2d.k2dmessenger.utilits.*
 
 class ChangeNameFragment : Fragment(R.layout.fragment_change_name) {
 
     override fun onResume() {
         super.onResume()
+        setHasOptionsMenu(true)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        (activity as MainActivity).menuInflater.inflate(R.menu.settings_menu_confirm,menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.settings_confirm_change -> changeName()
+        }
+        return true
+    }
+
+    private fun changeName() {
+        val name = settings_input_name.text.toString()
+        val surname = settings_input_surname.text.toString()
+        if (name.isEmpty()) showToast(getString(R.string.settings_toast_name_is_empty)) else {
+            val fullname = "$name $surname"
+            REF_DATABASE_ROOT.child(NODE_USERS).child(UID).child(CHILD_FULLNAME).setValue(fullname).addOnCompleteListener {
+                if (it.isSuccessful){
+                    showToast(getString(R.string.toast_data_update))
+                    USER.fullname = fullname
+                    fragmentManager?.popBackStack()
+                }
+            }
+        }
     }
 
 }
--- app/src/main/java/ru/k2d/k2dmessenger/utilits/firebaseHelper.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/utilits/firebaseHelper.kt
@@ -3,17 +3,23 @@ package ru.k2d.k2dmessenger.utilits
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import ru.k2d.k2dmessenger.models.User
 
 lateinit var AUTH: FirebaseAuth
+lateinit var UID: String
 lateinit var REF_DATABASE_ROOT: DatabaseReference
+lateinit var USER: User
 
 const val NODE_USERS = "users"
 const val CHILD_ID = "id"
 const val CHILD_PHONE = "phone"
 const val CHILD_USERNAME = "username"
+const val CHILD_FULLNAME = "fullname"
 
 
 fun initFirebase() {
     AUTH = FirebaseAuth.getInstance()
     REF_DATABASE_ROOT = FirebaseDatabase.getInstance().reference
+    USER = User()
+    UID = AUTH.currentUser?.uid.toString()
 }
