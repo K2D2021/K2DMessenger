439eb8d79ee15472ee35a70cb8daa7f24f5bbf0d
K2D2021 <zxtreme@yandex.ru>
2022-12-17 14:49:53 +0300

    Add contact_item.xml

--- app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/ContactsFragment.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/ContactsFragment.kt
@@ -1,10 +1,14 @@
 package ru.k2d.k2dmessenger.ui.fragments
 
 import ru.k2d.k2dmessenger.R
+import ru.k2d.k2dmessenger.utilits.APP_ACTIVITY
 
 
 class ContactsFragment : BaseFragment(R.layout.fragment_contacts) {
 
-
+    override fun onResume() {
+        super.onResume()
+        APP_ACTIVITY.title = "Contacts"
+    }
 
 }
