38def37f8de12d868cf94adea40de4e425c10d9d
K2D2021 <zxtreme@yandex.ru>
2023-01-01 12:51:50 +0300

    Add toolbar_info.xml

--- app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/SingleChatFragment.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/SingleChatFragment.kt
@@ -1,11 +1,14 @@
 package ru.k2d.k2dmessenger.ui.fragments
 
 import ru.k2d.k2dmessenger.R
+import ru.k2d.k2dmessenger.utilits.APP_ACTIVITY
 
 
 class SingleChatFragment : BaseFragment(R.layout.fragment_single_chat) {
 
 
-
+    override fun onResume() {
+        super.onResume()
+    }
 
 }
