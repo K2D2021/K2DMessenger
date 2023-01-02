44a4402d52f53acc1d733504d7e0de4b450273df
K2D2021 <zxtreme@yandex.ru>
2023-01-02 12:14:15 +0300

    Add logic to work with SingleChatFragment.kt

--- app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/SingleChatFragment.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/SingleChatFragment.kt
@@ -1,14 +1,24 @@
 package ru.k2d.k2dmessenger.ui.fragments
 
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 import ru.k2d.k2dmessenger.R
+import ru.k2d.k2dmessenger.models.CommonModel
 import ru.k2d.k2dmessenger.utilits.APP_ACTIVITY
 
 
-class SingleChatFragment : BaseFragment(R.layout.fragment_single_chat) {
+class SingleChatFragment(contact: CommonModel) : BaseFragment(R.layout.fragment_single_chat) {
 
 
     override fun onResume() {
         super.onResume()
+        APP_ACTIVITY.mToolbar.toolbar_info.visibility = View.VISIBLE
+    }
+
+    override fun onPause() {
+        super.onPause()
+        APP_ACTIVITY.mToolbar.toolbar_info.visibility = View.GONE
     }
 
 }
