816d1eedc726148a4575db367a820c3d0ec044a0
K2D2021 <zxtreme@yandex.ru>
2023-01-06 01:21:36 +0300

    Tune SingleChatFragment.kt

--- app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/SingleChatFragment.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/SingleChatFragment.kt
@@ -5,15 +5,21 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
 import ru.k2d.k2dmessenger.R
 import ru.k2d.k2dmessenger.models.CommonModel
+import ru.k2d.k2dmessenger.models.User
 import ru.k2d.k2dmessenger.utilits.APP_ACTIVITY
+import ru.k2d.k2dmessenger.utilits.AppValueEventListener
 
 
 class SingleChatFragment(contact: CommonModel) : BaseFragment(R.layout.fragment_single_chat) {
 
+    private lateinit var mListenerInfoToolbar: AppValueEventListener
+    private lateinit var mReceivingUser: User
+    private lateinit var mToolbarInfo: View
 
     override fun onResume() {
         super.onResume()
-        APP_ACTIVITY.mToolbar.toolbar_info.visibility = View.VISIBLE
+        mToolbarInfo = APP_ACTIVITY.mToolbar.toolbar_info
+        mToolbarInfo.visibility = View.VISIBLE
     }
 
     override fun onPause() {
