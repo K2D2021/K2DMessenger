289fb0dff6dbde6b4347fc0a110d32e135b60ba7
K2D2021 <zxtreme@yandex.ru>
2023-01-07 03:10:56 +0300

    Refactoring ChatsFragment.kt

--- app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/ChatsFragment.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/ChatsFragment.kt
@@ -2,10 +2,12 @@ package ru.k2d.k2dmessenger.ui.fragments
 
 import androidx.fragment.app.Fragment
 import ru.k2d.k2dmessenger.R
+import ru.k2d.k2dmessenger.utilits.APP_ACTIVITY
 
 class ChatsFragment : Fragment(R.layout.fragment_chats) {
 
     override fun onResume() {
         super.onResume()
+        APP_ACTIVITY.title = "Chats"
     }
 }
