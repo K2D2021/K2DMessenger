73a761b113c68f980422b4371ae2010b47f6ddc5
K2D2021 <zxtreme@yandex.ru>
2023-01-08 23:49:44 +0300

    Add logic to show correct side of messages

--- app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/single_chat/SingleChatAdapter.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/single_chat/SingleChatAdapter.kt
@@ -1,12 +1,21 @@
 package ru.k2d.k2dmessenger.ui.fragments.single_chat
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.message_item.view.*
+import ru.k2d.k2dmessenger.R
+import ru.k2d.k2dmessenger.models.CommonModel
+import ru.k2d.k2dmessenger.utilits.CURRENT_UID
+import java.text.SimpleDateFormat
+import java.util.*
 
-class SingleChatAdapter: RecyclerView.Adapter<> {
+class SingleChatAdapter: RecyclerView.Adapter<SingleChatAdapter.SingleChatHolder>() {
+
+    private var mlistMessagesCache = emptyList<CommonModel>()
 
     class SingleChatHolder(view: View) : RecyclerView.ViewHolder(view){
         val blocUserMessage:ConstraintLayout = view.bloc_user_message
@@ -18,4 +27,34 @@ class SingleChatAdapter: RecyclerView.Adapter<> {
         val chatReceivedMessageTime:TextView = view.chat_received_message_time
     }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SingleChatHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.message_item,parent,false)
+        return SingleChatHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: SingleChatHolder, position: Int) {
+        if(mlistMessagesCache[position].from == CURRENT_UID){
+            holder.blocUserMessage.visibility = View.VISIBLE
+            holder.blocReceivedMessage.visibility = View.GONE
+            holder.chatUserMessage.text = mlistMessagesCache[position].text
+            holder.chatUserMessageTime.text = mlistMessagesCache[position].timeStamp.asTime()
+        } else {
+            holder.blocUserMessage.visibility = View.GONE
+            holder.blocReceivedMessage.visibility = View.VISIBLE
+            holder.chatReceivedMessage.text = mlistMessagesCache[position].text
+            holder.chatReceivedMessageTime.text = mlistMessagesCache[position].timeStamp.asTime()
+        }
+    }
+
+    override fun getItemCount(): Int = mlistMessagesCache.size
+
+    fun setList(list: List<CommonModel>){
+        mlistMessagesCache = list
+    }
+}
+
+private fun String.asTime(): String {
+    val time = Date(this.toLong())
+    val timeFormat = SimpleDateFormat("HH:MM",Locale.getDefault())
+    return timeFormat.format(time)
 }
