fc7ed6e34b20943f04cd2c8e84beb4126768264f
K2D2021 <zxtreme@yandex.ru>
2023-01-08 23:31:28 +0300

    Add SingleChatHolder

--- app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/single_chat/SingleChatAdapter.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/single_chat/SingleChatAdapter.kt
@@ -1,4 +1,21 @@
 package ru.k2d.k2dmessenger.ui.fragments.single_chat
 
-class SingleChatAdapter {
+import android.view.View
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.message_item.view.*
+
+class SingleChatAdapter: RecyclerView.Adapter<> {
+
+    class SingleChatHolder(view: View) : RecyclerView.ViewHolder(view){
+        val blocUserMessage:ConstraintLayout = view.bloc_user_message
+        val chatUserMessage:TextView = view.chat_user_message
+        val chatUserMessageTime:TextView = view.chat_user_message_time
+
+        val blocReceivedMessage:ConstraintLayout = view.bloc_received_message
+        val chatReceivedMessage:TextView = view.chat_received_message
+        val chatReceivedMessageTime:TextView = view.chat_received_message_time
+    }
+
 }
