0b0c871ef30f8ad4f6276c8a0ac87c8e62e60c1d
K2D2021 <zxtreme@yandex.ru>
2023-01-13 01:57:27 +0300

    Refactoring

--- app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/single_chat/SingleChatAdapter.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/single_chat/SingleChatAdapter.kt
@@ -15,50 +15,51 @@ import ru.k2d.k2dmessenger.models.CommonModel
 import ru.k2d.k2dmessenger.utilits.DiffUtilCallback
 import ru.k2d.k2dmessenger.utilits.asTime
 
-class SingleChatAdapter: RecyclerView.Adapter<SingleChatAdapter.SingleChatHolder>() {
+class SingleChatAdapter : RecyclerView.Adapter<SingleChatAdapter.SingleChatHolder>() {
 
     private var mlistMessagesCache = emptyList<CommonModel>()
     private lateinit var mDiffResult: DiffResult
 
-    class SingleChatHolder(view: View) : RecyclerView.ViewHolder(view){
-        val blocUserMessage:ConstraintLayout = view.bloc_user_message
-        val chatUserMessage:TextView = view.chat_user_message
-        val chatUserMessageTime:TextView = view.chat_user_message_time
+    class SingleChatHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val blocUserMessage: ConstraintLayout = view.bloc_user_message
+        val chatUserMessage: TextView = view.chat_user_message
+        val chatUserMessageTime: TextView = view.chat_user_message_time
 
-        val blocReceivedMessage:ConstraintLayout = view.bloc_received_message
-        val chatReceivedMessage:TextView = view.chat_received_message
-        val chatReceivedMessageTime:TextView = view.chat_received_message_time
+        val blocReceivedMessage: ConstraintLayout = view.bloc_received_message
+        val chatReceivedMessage: TextView = view.chat_received_message
+        val chatReceivedMessageTime: TextView = view.chat_received_message_time
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SingleChatHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.message_item,parent,false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.message_item, parent, false)
         return SingleChatHolder(view)
     }
 
     override fun onBindViewHolder(holder: SingleChatHolder, position: Int) {
-        if(mlistMessagesCache[position].from == CURRENT_UID){
+        if (mlistMessagesCache[position].from == CURRENT_UID) {
             holder.blocUserMessage.visibility = View.VISIBLE
             holder.blocReceivedMessage.visibility = View.GONE
             holder.chatUserMessage.text = mlistMessagesCache[position].text
-            holder.chatUserMessageTime.text = mlistMessagesCache[position].timeStamp.toString().asTime()
+            holder.chatUserMessageTime.text =
+                mlistMessagesCache[position].timeStamp.toString().asTime()
         } else {
             holder.blocUserMessage.visibility = View.GONE
             holder.blocReceivedMessage.visibility = View.VISIBLE
             holder.chatReceivedMessage.text = mlistMessagesCache[position].text
-            holder.chatReceivedMessageTime.text = mlistMessagesCache[position].timeStamp.toString().asTime()
+            holder.chatReceivedMessageTime.text =
+                mlistMessagesCache[position].timeStamp.toString().asTime()
         }
     }
 
     override fun getItemCount(): Int = mlistMessagesCache.size
 
-    fun setList(list: List<CommonModel>){
-
-    }
-
-    fun addItem(item: CommonModel){
+    fun addItem(item: CommonModel) {
         val newList = mutableListOf<CommonModel>()
         newList.addAll(mlistMessagesCache)
-        newList.add(item)
+        if (!newList.contains(item)) newList.add(item)
+
+        newList.sortBy { it.timeStamp.toString() }
+
         mDiffResult = DiffUtil.calculateDiff(DiffUtilCallback(mlistMessagesCache, newList))
         mDiffResult.dispatchUpdatesTo(this)
         mlistMessagesCache = newList
