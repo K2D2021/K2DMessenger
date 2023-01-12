75b135a6c90dc419b3b1b998db29bb057a04a744
K2D2021 <zxtreme@yandex.ru>
2023-01-12 21:48:29 +0300

    Refactoring

--- app/src/main/java/ru/k2d/k2dmessenger/utilits/DiffUtilCallback.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/utilits/DiffUtilCallback.kt
@@ -3,7 +3,10 @@ package ru.k2d.k2dmessenger.utilits
 import androidx.recyclerview.widget.DiffUtil
 import ru.k2d.k2dmessenger.models.CommonModel
 
-class DiffUtilCallback(private val oldList: List<CommonModel>, private val newList: List<CommonModel>):DiffUtil.Callback() {
+class DiffUtilCallback(
+    private val oldList: List<CommonModel>,
+    private val newList: List<CommonModel>
+) : DiffUtil.Callback() {
     override fun getOldListSize(): Int {
 
     }
