5e5d1504547dbcb198f706a98197db2a4db87e41
K2D2021 <zxtreme@yandex.ru>
2023-01-13 00:25:52 +0300

    Complete logic part of better way to show messages

--- app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/single_chat/SingleChatFragment.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/single_chat/SingleChatFragment.kt
@@ -3,8 +3,6 @@ package ru.k2d.k2dmessenger.ui.fragments.single_chat
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.database.ChildEventListener
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.fragment_single_chat.*
@@ -41,28 +39,9 @@ class SingleChatFragment(private val contact: CommonModel) :
         mAdapter = SingleChatAdapter()
         mRefMessages = REF_DATABASE_ROOT.child(NODE_MESSAGES).child(CURRENT_UID).child(contact.id)
         mRecyclerView.adapter = mAdapter
-        mMessagesListener = object : ChildEventListener {
-            override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
-                mAdapter.addItem(snapshot.getCommonModel())
-                mRecyclerView.smoothScrollToPosition(mAdapter.itemCount)
-            }
-
-            override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {
-                //TODO("Not yet implemented")
-            }
-
-            override fun onChildRemoved(snapshot: DataSnapshot) {
-//                TODO("Not yet implemented")
-            }
-
-            override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {
-//                TODO("Not yet implemented")
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-//                TODO("Not yet implemented")
-            }
-
+        mMessagesListener = AppChildEventListener{
+            mAdapter.addItem(it.getCommonModel())
+            mRecyclerView.smoothScrollToPosition(mAdapter.itemCount)
         }
 
         mRefMessages.addChildEventListener(mMessagesListener)
