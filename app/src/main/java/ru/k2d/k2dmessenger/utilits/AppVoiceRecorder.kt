e8032c10e36d6c140b44890c20b0591e4373cc3c
K2D2021 <zxtreme@yandex.ru>
2023-01-23 11:45:12 +0300

    Add media recorder logic.

--- app/src/main/java/ru/k2d/k2dmessenger/utilits/AppVoiceRecorder.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/utilits/AppVoiceRecorder.kt
@@ -4,26 +4,44 @@ import android.media.MediaRecorder
 import java.io.File
 
 class AppVoiceRecorder {
-    companion object{
+    companion object {
 
         private val mMediaRecorder = MediaRecorder()
         private lateinit var mFile: File
+        private lateinit var mMessageKey: String
 
 
+        fun startRecord(messageKey: String) {
+            try {
+                mMessageKey = messageKey
+                createFileForRecord()
+                prepareMediaRecorder()
+                mMediaRecorder.start()
+            } catch (e: Exception) {
+                showToast(e.message.toString())
+            }
+        }
 
-        fun startRecord(){
-            createFileForRecord()
+        private fun prepareMediaRecorder() {
+            mMediaRecorder.reset()
+            mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.DEFAULT)
+            mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.DEFAULT)
+            mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT)
+            mMediaRecorder.setOutputFile(mFile.absolutePath)
+            mMediaRecorder.prepare()
         }
 
         private fun createFileForRecord() {
+            mFile = File(APP_ACTIVITY.filesDir, mMessageKey)
+            mFile.createNewFile()
 
         }
 
-        fun stopRecord(onSuccess:() -> Unit){
+        fun stopRecord(onSuccess: () -> Unit) {
 
         }
 
-        fun releaseRecorder(){
+        fun releaseRecorder() {
 
         }
     }
