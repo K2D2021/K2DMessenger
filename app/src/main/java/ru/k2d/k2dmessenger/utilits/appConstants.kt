0dd98b9b2be576a026621846ed178b86eca770cc
K2D2021 <zxtreme@yandex.ru>
2023-01-23 09:53:36 +0300

    Add new constant.

--- app/src/main/java/ru/k2d/k2dmessenger/utilits/appConstants.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/utilits/appConstants.kt
@@ -5,3 +5,4 @@ import ru.k2d.k2dmessenger.MainActivity
 lateinit var APP_ACTIVITY:MainActivity
 const val TYPE_MESSAGE_IMAGE = "image"
 const val TYPE_MESSAGE_TEXT = "text"
+const val TYPE_MESSAGE_VOICE = "voice"
