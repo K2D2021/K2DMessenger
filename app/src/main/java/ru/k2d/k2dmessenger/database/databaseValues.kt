12b9c96fd9c5e0766a7ab572ca19c87dd0589a6b
K2D2021 <zxtreme@yandex.ru>
2023-01-24 23:38:59 +0300

    Move for split constants and funs from appDatabaseHelper.kt

--- app/src/main/java/ru/k2d/k2dmessenger/database/databaseValues.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/database/databaseValues.kt
@@ -1,2 +1,34 @@
 package ru.k2d.k2dmessenger.database
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.storage.StorageReference
+import ru.k2d.k2dmessenger.models.Usermodel
+
+lateinit var AUTH: FirebaseAuth
+lateinit var CURRENT_UID: String
+lateinit var REF_DATABASE_ROOT: DatabaseReference
+lateinit var REF_STORAGE_ROOT: StorageReference
+lateinit var USER: Usermodel
+
+const val TYPE_TEXT = "text"
+const val NODE_USERS = "users"
+const val NODE_USERNAMES = "usernames"
+const val NODE_PHONES = "phones"
+const val NODE_PHONES_CONTACTS = "phones_contacts"
+const val NODE_MESSAGES = "messages"
+const val FOLDER_PROFILE_IMAGE = "profile_image"
+const val FOLDER_MESSAGE_IMAGE = "message_image"
+const val FOLDER_FILES = "files"
+const val CHILD_ID = "id"
+const val CHILD_PHONE = "phone"
+const val CHILD_USERNAME = "username"
+const val CHILD_FULLNAME = "fullname"
+const val CHILD_BIO = "bio"
+const val CHILD_PHOTO_URL = "photoUrl"
+const val CHILD_STATE = "state"
+const val CHILD_TEXT = "text"
+const val CHILD_TYPE = "type"
+const val CHILD_FROM = "from"
+const val CHILD_TIMESTAMP = "timeStamp"
+const val CHILD_FILE_URL = "fileUrl"
