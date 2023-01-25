8e0138e56cdb8b0e3a5eac3f9e43fc7dec8d26a3
K2D2021 <zxtreme@yandex.ru>
2023-01-26 01:05:00 +0300

    Add ViewVoiceMessage.kt

---
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/fragments/message_recycler_view/views/ViewVoiceMessage.kt
@@ -0,0 +1,17 @@
+package ru.k2d.k2dmessenger.ui.fragments.message_recycler_view.views
+
+data class ViewVoiceMessage(
+    override val id: String,
+    override val from: String,
+    override val timeStamp: String,
+    override val fileUrl: String,
+    override val text: String = ""
+) : MessageView {
+    override fun getTypeView(): Int {
+        return MessageView.MESSAGE_IMAGE
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return (other as MessageView).id == id
+    }
+}
