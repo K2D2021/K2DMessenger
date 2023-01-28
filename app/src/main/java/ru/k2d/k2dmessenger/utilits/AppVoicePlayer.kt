9e257882bf0c6baf5fd4fe0d5e0c2b3001b7176a
K2D2021 <zxtreme@yandex.ru>
2023-01-28 23:46:55 +0300

    Add funs (play, stop, release) to AppVoicePlayer.kt

--- app/src/main/java/ru/k2d/k2dmessenger/utilits/AppVoicePlayer.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/utilits/AppVoicePlayer.kt
@@ -1,4 +1,21 @@
 package ru.k2d.k2dmessenger.utilits
 
+import android.media.MediaPlayer
+import java.io.File
+
 class AppVoicePlayer {
+    private lateinit var mMediaPlayer: MediaPlayer
+    private lateinit var mFile: File
+
+    fun play(){
+
+    }
+
+    fun stop(){
+
+    }
+
+    fun release(){
+
+    }
 }
