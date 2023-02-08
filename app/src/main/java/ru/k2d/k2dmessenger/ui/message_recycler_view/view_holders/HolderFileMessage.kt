2dafedd8f624152a1954339631786fbe361dbccb
K2D2021 <zxtreme@yandex.ru>
2023-02-09 01:56:44 +0300

    Add variables for use progress bar in HolderFileMessage.kt

--- app/src/main/java/ru/k2d/k2dmessenger/ui/message_recycler_view/view_holders/HolderFileMessage.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/message_recycler_view/view_holders/HolderFileMessage.kt
@@ -1,6 +1,8 @@
 package ru.k2d.k2dmessenger.ui.message_recycler_view.view_holders
 
 import android.view.View
+import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
@@ -16,7 +18,13 @@ class HolderFileMessage(view: View) : RecyclerView.ViewHolder(view), MessageHold
     private val chatReceivedFileMessageTime: TextView = view.chat_received_file_message_time
     private val chatUserFileMessageTime: TextView = view.chat_user_file_message_time
 
+    private val chatUserFilename: TextView = view.chat_user_file_name
+    private val chatUserBtnDownload : ImageView = view.chat_user_btn_download
+    private val chatUserProgressBar: ProgressBar = view.chat_user_progress_bar
 
+    private val chatReceivedFilename: TextView = view.chat_received_file_name
+    private val chatReceivedBtnDownload : ImageView = view.chat_received_btn_download
+    private val chatReceivedProgressBar: ProgressBar = view.chat_received_progress_bar
 
     override fun drawMessage(view: MessageView) {
         if (view.from == CURRENT_UID) {
