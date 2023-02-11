56fc6f0b6d965e8266790a0b808b8930250a2ceb
K2D2021 <zxtreme@yandex.ru>
2023-02-11 23:49:01 +0300

    Add recyclerView to MainListFragment.kt

--- app/src/main/java/ru/k2d/k2dmessenger/ui/screens/main_list/MainListFragment.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/screens/main_list/MainListFragment.kt
@@ -1,16 +1,28 @@
 package ru.k2d.k2dmessenger.ui.screens.main_list
 
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_main_list.*
 import ru.k2d.k2dmessenger.R
 import ru.k2d.k2dmessenger.utilits.APP_ACTIVITY
 import ru.k2d.k2dmessenger.utilits.hideKeyboard
 
 class MainListFragment : Fragment(R.layout.fragment_main_list) {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var mAdapter: MainListAdapter
+
     override fun onResume() {
         super.onResume()
         APP_ACTIVITY.title = "K2DMessenger"
         APP_ACTIVITY.mAppDrawer.enableDrawer()
         hideKeyboard()
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        recyclerView = main_list_recycle_view
+        mAdapter = MainListAdapter()
     }
 }
