392e4aa0b4352fc52455999d19d6901f1a935b83
K2D2021 <zxtreme@yandex.ru>
2023-02-12 00:08:09 +0300

    Complete logic for showing elements in recyclerView on main screen

--- app/src/main/java/ru/k2d/k2dmessenger/ui/screens/main_list/MainListFragment.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/screens/main_list/MainListFragment.kt
@@ -4,13 +4,21 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_main_list.*
 import ru.k2d.k2dmessenger.R
+import ru.k2d.k2dmessenger.database.*
+import ru.k2d.k2dmessenger.models.CommonModel
 import ru.k2d.k2dmessenger.utilits.APP_ACTIVITY
+import ru.k2d.k2dmessenger.utilits.AppValueEventListener
 import ru.k2d.k2dmessenger.utilits.hideKeyboard
 
 class MainListFragment : Fragment(R.layout.fragment_main_list) {
 
-    private lateinit var recyclerView: RecyclerView
+    private lateinit var mRecyclerView: RecyclerView
     private lateinit var mAdapter: MainListAdapter
+
+    private val mRefMainList = REF_DATABASE_ROOT.child(NODE_MAIN_LIST).child(CURRENT_UID)
+    private val mRefUsers = REF_DATABASE_ROOT.child(NODE_USERS)
+    private val mRefMessages = REF_DATABASE_ROOT.child(NODE_MESSAGES).child(CURRENT_UID)
+    private var mListItems = listOf<CommonModel>()
 
     override fun onResume() {
         super.onResume()
@@ -22,7 +30,28 @@ class MainListFragment : Fragment(R.layout.fragment_main_list) {
     }
 
     private fun initRecyclerView() {
-        recyclerView = main_list_recycle_view
+        mRecyclerView = main_list_recycle_view
         mAdapter = MainListAdapter()
+        mRefMainList.addListenerForSingleValueEvent(AppValueEventListener { dataSnapshot ->
+            mListItems = dataSnapshot.children.map { it.getCommonModel() }
+            mListItems.forEach { model ->
+                mRefUsers.child(model.id)
+                    .addListenerForSingleValueEvent(AppValueEventListener { dataSnapshot1 ->
+                        val newModel = dataSnapshot1.getCommonModel()
+                        mRefMessages.child(model.id).limitToLast(1)
+                            .addListenerForSingleValueEvent(AppValueEventListener { dataSnapshot2 ->
+                                val tempList = dataSnapshot2.children.map { it.getCommonModel() }
+                                newModel.lastMessage = tempList[0].lastMessage
+                                if (newModel.fullname.isEmpty()){
+                                    newModel.fullname = newModel.phone
+                                }
+                                mAdapter.updateListItems(newModel)
+                            })
+                    })
+
+            }
+        })
+
+        mRecyclerView.adapter = mAdapter
     }
 }
