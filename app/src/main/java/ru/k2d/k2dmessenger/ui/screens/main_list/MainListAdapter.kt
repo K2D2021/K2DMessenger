ef2a4b1dc38e85eed8d8b183c0b9d0d9108d6ff0
K2D2021 <zxtreme@yandex.ru>
2023-02-13 23:38:29 +0300

    Add listener to open single chat from main screen.

--- app/src/main/java/ru/k2d/k2dmessenger/ui/screens/main_list/MainListAdapter.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/screens/main_list/MainListAdapter.kt
@@ -9,7 +9,9 @@ import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.synthetic.main.main_list_item.view.*
 import ru.k2d.k2dmessenger.R
 import ru.k2d.k2dmessenger.models.CommonModel
+import ru.k2d.k2dmessenger.ui.screens.single_chat.SingleChatFragment
 import ru.k2d.k2dmessenger.utilits.downloadAndSetImage
+import ru.k2d.k2dmessenger.utilits.replaceFragment
 
 class MainListAdapter : RecyclerView.Adapter<MainListAdapter.MainListHolder>() {
 
@@ -17,7 +19,11 @@ class MainListAdapter : RecyclerView.Adapter<MainListAdapter.MainListHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainListHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.main_list_item,parent,false)
-        return MainListHolder(view)
+        val holder = MainListHolder(view)
+        holder.itemView.setOnClickListener {
+            replaceFragment(SingleChatFragment(listItems[holder.adapterPosition]))
+        }
+        return holder
     }
 
     override fun onBindViewHolder(holder: MainListHolder, position: Int) {
