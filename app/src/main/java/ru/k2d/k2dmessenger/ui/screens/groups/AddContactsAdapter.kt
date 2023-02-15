d8437d8aa8ca89a3db548bb8af83ddc9c55136c5
K2D2021 <zxtreme@yandex.ru>
2023-02-16 00:06:00 +0300

    Add new package groups and add to it AddContactsAdapter.kt and AddContactsFragment.kt.

---
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/screens/groups/AddContactsAdapter.kt
@@ -0,0 +1,47 @@
+package ru.k2d.k2dmessenger.ui.screens.groups
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import de.hdodenhof.circleimageview.CircleImageView
+import kotlinx.android.synthetic.main.main_list_item.view.*
+import ru.k2d.k2dmessenger.R
+import ru.k2d.k2dmessenger.models.CommonModel
+import ru.k2d.k2dmessenger.ui.screens.single_chat.SingleChatFragment
+import ru.k2d.k2dmessenger.utilits.downloadAndSetImage
+import ru.k2d.k2dmessenger.utilits.replaceFragment
+
+class AddContactsAdapter : RecyclerView.Adapter<AddContactsAdapter.MainListHolder>() {
+
+    private var listItems = mutableListOf<CommonModel>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainListHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.main_list_item,parent,false)
+        val holder = MainListHolder(view)
+        holder.itemView.setOnClickListener {
+            replaceFragment(SingleChatFragment(listItems[holder.adapterPosition]))
+        }
+        return holder
+    }
+
+    override fun onBindViewHolder(holder: MainListHolder, position: Int) {
+        holder.itemName.text = listItems[position].fullname
+        holder.itemLastMessage.text = listItems[position].lastMessage
+        holder.itemPhoto.downloadAndSetImage(listItems[position].photoUrl)
+    }
+
+    fun updateListItems(item:CommonModel){
+        listItems.add(item)
+        notifyItemInserted(listItems.size)
+    }
+
+    override fun getItemCount(): Int = listItems.size
+
+    class MainListHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val itemName: TextView = view.main_list_item_name
+        val itemLastMessage: TextView = view.main_list_last_message
+        val itemPhoto: CircleImageView = view.main_list_item_photo
+    }
+}
---
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/screens/groups/AddContactsFragment.kt
@@ -0,0 +1,62 @@
+package ru.k2d.k2dmessenger.ui.screens.groups
+
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_main_list.*
+import ru.k2d.k2dmessenger.R
+import ru.k2d.k2dmessenger.database.*
+import ru.k2d.k2dmessenger.models.CommonModel
+import ru.k2d.k2dmessenger.utilits.APP_ACTIVITY
+import ru.k2d.k2dmessenger.utilits.AppValueEventListener
+import ru.k2d.k2dmessenger.utilits.hideKeyboard
+
+class AddContactsFragment : Fragment(R.layout.fragment_main_list) {
+
+    private lateinit var mRecyclerView: RecyclerView
+    private lateinit var mAdapter: AddContactsAdapter
+
+    private val mRefMainList = REF_DATABASE_ROOT.child(NODE_MAIN_LIST).child(CURRENT_UID)
+    private val mRefUsers = REF_DATABASE_ROOT.child(NODE_USERS)
+    private val mRefMessages = REF_DATABASE_ROOT.child(NODE_MESSAGES).child(CURRENT_UID)
+    private var mListItems = listOf<CommonModel>()
+
+    override fun onResume() {
+        super.onResume()
+        APP_ACTIVITY.title = "K2DMessenger"
+        APP_ACTIVITY.mAppDrawer.enableDrawer()
+        hideKeyboard()
+
+        initRecyclerView()
+    }
+
+    private fun initRecyclerView() {
+        mRecyclerView = main_list_recycle_view
+        mAdapter = AddContactsAdapter()
+        mRefMainList.addListenerForSingleValueEvent(AppValueEventListener { dataSnapshot ->
+            mListItems = dataSnapshot.children.map { it.getCommonModel() }
+            mListItems.forEach { model ->
+                mRefUsers.child(model.id)
+                    .addListenerForSingleValueEvent(AppValueEventListener { dataSnapshot1 ->
+                        val newModel = dataSnapshot1.getCommonModel()
+                        mRefMessages.child(model.id).limitToLast(1)
+                            .addListenerForSingleValueEvent(AppValueEventListener { dataSnapshot2 ->
+                                val tempList = dataSnapshot2.children.map { it.getCommonModel() }
+
+                                if (tempList.isEmpty()) {
+                                    newModel.lastMessage = "Chat is cleared"
+                                } else {
+                                    newModel.lastMessage = tempList[0].text
+                                }
+                                if (newModel.fullname.isEmpty()) {
+                                    newModel.fullname = newModel.phone
+                                }
+                                mAdapter.updateListItems(newModel)
+                            })
+                    })
+
+            }
+        })
+
+        mRecyclerView.adapter = mAdapter
+    }
+}
