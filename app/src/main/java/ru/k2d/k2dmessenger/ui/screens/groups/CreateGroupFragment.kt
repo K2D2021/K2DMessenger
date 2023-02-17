bcd60f3a76a17a6e9570e0193476f9b7a8b20cf6
K2D2021 <zxtreme@yandex.ru>
2023-02-17 16:46:56 +0300

    Add CreateGroupFragment.kt

---
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/screens/groups/CreateGroupFragment.kt
@@ -0,0 +1,9 @@
+package ru.k2d.k2dmessenger.ui.screens.groups
+
+import ru.k2d.k2dmessenger.R
+import ru.k2d.k2dmessenger.models.CommonModel
+import ru.k2d.k2dmessenger.ui.screens.base.BaseFragment
+
+class CreateGroupFragment(var listContacts:List<CommonModel>) : BaseFragment(R.layout.fragment_create_group) {
+
+}
