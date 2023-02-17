73987e5197d8a2f28752b10c9dfa93284d929957
K2D2021 <zxtreme@yandex.ru>
2023-02-17 16:54:28 +0300

    Complete logic for CreateGroupFragment.kt.

--- app/src/main/java/ru/k2d/k2dmessenger/ui/screens/groups/CreateGroupFragment.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/screens/groups/CreateGroupFragment.kt
@@ -1,9 +1,36 @@
 package ru.k2d.k2dmessenger.ui.screens.groups
 
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_add_contacts.*
+import kotlinx.android.synthetic.main.fragment_create_group.*
 import ru.k2d.k2dmessenger.R
+import ru.k2d.k2dmessenger.database.*
 import ru.k2d.k2dmessenger.models.CommonModel
 import ru.k2d.k2dmessenger.ui.screens.base.BaseFragment
+import ru.k2d.k2dmessenger.utilits.*
 
-class CreateGroupFragment(var listContacts:List<CommonModel>) : BaseFragment(R.layout.fragment_create_group) {
+class CreateGroupFragment(private var listContacts: List<CommonModel>) :
+    BaseFragment(R.layout.fragment_create_group) {
 
+    private lateinit var mRecyclerView: RecyclerView
+    private lateinit var mAdapter: AddContactsAdapter
+
+    override fun onResume() {
+        super.onResume()
+        APP_ACTIVITY.title = getString(R.string.create_group)
+        APP_ACTIVITY.mAppDrawer.enableDrawer()
+        hideKeyboard()
+        initRecyclerView()
+        create_group_btn_complete.setOnClickListener { showToast("Click") }
+    }
+
+    private fun initRecyclerView() {
+        mRecyclerView = create_group_recycle_view
+        mAdapter = AddContactsAdapter()
+
+        mRecyclerView.adapter = mAdapter
+        listContacts.forEach {
+            mAdapter.updateListItems(it)
+        }
+    }
 }
