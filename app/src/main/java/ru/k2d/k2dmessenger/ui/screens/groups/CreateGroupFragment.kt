3157a4e1fa47703e081bdffb8c2cc684db447f3b
K2D2021 <zxtreme@yandex.ru>
2023-02-18 23:24:20 +0300

    Complete fun addPhoto to CreateGroupFragment.kt for create photo of the group.

--- app/src/main/java/ru/k2d/k2dmessenger/ui/screens/groups/CreateGroupFragment.kt
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/screens/groups/CreateGroupFragment.kt
@@ -1,17 +1,18 @@
 package ru.k2d.k2dmessenger.ui.screens.groups
 
+import android.app.Activity
+import android.content.Intent
 import android.net.Uri
 import androidx.recyclerview.widget.RecyclerView
 import com.theartofdev.edmodo.cropper.CropImage
 import com.theartofdev.edmodo.cropper.CropImageView
 import kotlinx.android.synthetic.main.fragment_create_group.*
+import kotlinx.android.synthetic.main.fragment_settings.*
 import ru.k2d.k2dmessenger.R
+import ru.k2d.k2dmessenger.database.*
 import ru.k2d.k2dmessenger.models.CommonModel
 import ru.k2d.k2dmessenger.ui.screens.base.BaseFragment
-import ru.k2d.k2dmessenger.utilits.APP_ACTIVITY
-import ru.k2d.k2dmessenger.utilits.getPlurals
-import ru.k2d.k2dmessenger.utilits.hideKeyboard
-import ru.k2d.k2dmessenger.utilits.showToast
+import ru.k2d.k2dmessenger.utilits.*
 
 class CreateGroupFragment(private var listContacts: List<CommonModel>) :
     BaseFragment(R.layout.fragment_create_group) {
@@ -46,6 +47,14 @@ class CreateGroupFragment(private var listContacts: List<CommonModel>) :
         mRecyclerView.adapter = mAdapter
         listContacts.forEach {
             mAdapter.updateListItems(it)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null) {
+            mUri = CropImage.getActivityResult(data).uri
+            create_group_photo.setImageURI(mUri)
         }
     }
 }
