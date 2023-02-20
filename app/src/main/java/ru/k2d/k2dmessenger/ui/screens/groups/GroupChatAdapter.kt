07aee119d23b6efff2cd19400bb200933a51abbc
K2D2021 <zxtreme@yandex.ru>
2023-02-20 12:46:13 +0300

    Add GroupChat Adapter and Fragment for work with group chats.

---
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/screens/groups/GroupChatAdapter.kt
@@ -0,0 +1,61 @@
+package ru.k2d.k2dmessenger.ui.screens.groups
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import ru.k2d.k2dmessenger.ui.message_recycler_view.view_holders.*
+import ru.k2d.k2dmessenger.ui.message_recycler_view.views.MessageView
+
+class GroupChatAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private var mlistMessagesCache = mutableListOf<MessageView>()
+    private var mListHolders = mutableListOf<MessageHolder>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return AppHolderFactory.getHolder(parent, viewType)
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return mlistMessagesCache[position].getTypeView()
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as MessageHolder).drawMessage(mlistMessagesCache[position])
+    }
+
+    override fun onViewAttachedToWindow(holder: RecyclerView.ViewHolder) {
+        (holder as MessageHolder).onAttach(mlistMessagesCache[holder.absoluteAdapterPosition])
+        mListHolders.add((holder as MessageHolder))
+        super.onViewAttachedToWindow(holder)
+    }
+
+    override fun onViewDetachedFromWindow(holder: RecyclerView.ViewHolder) {
+        (holder as MessageHolder).onDetach()
+        mListHolders.remove((holder as MessageHolder))
+        super.onViewDetachedFromWindow(holder)
+    }
+
+    override fun getItemCount(): Int = mlistMessagesCache.size
+
+    fun addItemToBottom(item: MessageView, onSuccess: () -> Unit) {
+        if (!mlistMessagesCache.contains(item)) {
+            mlistMessagesCache.add(item)
+            notifyItemInserted(mlistMessagesCache.size)
+        }
+        onSuccess()
+    }
+
+    fun addItemToTop(item: MessageView, onSuccess: () -> Unit) {
+        if (!mlistMessagesCache.contains(item)) {
+            mlistMessagesCache.add(item)
+            mlistMessagesCache.sortBy { it.timeStamp }
+            notifyItemInserted(0)
+        }
+        onSuccess()
+    }
+
+    fun onDestroy() {
+        mListHolders.forEach {
+            it.onDetach()
+        }
+    }
+}
+
---
+++ app/src/main/java/ru/k2d/k2dmessenger/ui/screens/groups/GroupChatFragment.kt
@@ -0,0 +1,270 @@
+package ru.k2d.k2dmessenger.ui.screens.groups
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
+import android.view.*
+import android.widget.AbsListView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.firebase.database.DatabaseReference
+import com.theartofdev.edmodo.cropper.CropImage
+import kotlinx.android.synthetic.main.activity_main.view.*
+import kotlinx.android.synthetic.main.choice_upload.*
+import kotlinx.android.synthetic.main.fragment_single_chat.*
+import kotlinx.android.synthetic.main.toolbar_info.view.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import ru.k2d.k2dmessenger.R
+import ru.k2d.k2dmessenger.database.*
+import ru.k2d.k2dmessenger.models.CommonModel
+import ru.k2d.k2dmessenger.models.Usermodel
+import ru.k2d.k2dmessenger.ui.message_recycler_view.views.AppViewFactory
+import ru.k2d.k2dmessenger.ui.screens.base.BaseFragment
+import ru.k2d.k2dmessenger.ui.screens.main_list.MainListFragment
+import ru.k2d.k2dmessenger.utilits.*
+
+
+class GroupChatFragment(private val contact: CommonModel) :
+    BaseFragment(R.layout.fragment_single_chat) {
+
+    private lateinit var mListenerInfoToolbar: AppValueEventListener
+    private lateinit var mReceivingUser: Usermodel
+    private lateinit var mToolbarInfo: View
+    private lateinit var mRefUser: DatabaseReference
+    private lateinit var mRefMessages: DatabaseReference
+    private lateinit var mAdapter: GroupChatAdapter
+    private lateinit var mRecyclerView: RecyclerView
+    private lateinit var mMessagesListener: AppChildEventListener
+    private var mCountMessages = 15
+    private var mIsScrolling = false
+    private var mSmoothScrollToPosition = true
+    private lateinit var mSwipeRefreshLayout: SwipeRefreshLayout
+    private lateinit var mLayoutManager: LinearLayoutManager
+    private lateinit var mAppVoiceRecorder: AppVoiceRecorder
+    private lateinit var mBottomSheetBehavior: BottomSheetBehavior<*>
+
+    override fun onResume() {
+        super.onResume()
+        initFields()
+        initToolbar()
+        initRecyclerView()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun initFields() {
+        setHasOptionsMenu(true)
+        mBottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet_choice)
+        mBottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+        mAppVoiceRecorder = AppVoiceRecorder()
+        mSwipeRefreshLayout = chat_swipe_refresh
+        mLayoutManager = LinearLayoutManager(this.context)
+        chat_input_message.addTextChangedListener(AppTextWatcher {
+            val string = chat_input_message.text.toString()
+            if (string.isEmpty() || string == "Recording") {
+                chat_btn_send_message.visibility = View.GONE
+                chat_btn_attach.visibility = View.VISIBLE
+                chat_btn_voice.visibility = View.VISIBLE
+            } else {
+                chat_btn_send_message.visibility = View.VISIBLE
+                chat_btn_attach.visibility = View.GONE
+                chat_btn_voice.visibility = View.GONE
+            }
+        })
+
+        chat_btn_attach.setOnClickListener { attach() }
+
+        CoroutineScope(Dispatchers.IO).launch {
+            chat_btn_voice.setOnTouchListener { v, event ->
+                if (checkPermissions(RECORD_AUDIO)) {
+                    if (event.action == MotionEvent.ACTION_DOWN) {
+                        chat_input_message.setText(getString(R.string.text_recording))
+                        chat_btn_voice.setColorFilter(
+                            ContextCompat.getColor(
+                                APP_ACTIVITY,
+                                R.color.colorPrimary
+                            )
+                        )
+                        val messageKey = getMessageKey(contact.id)
+                        mAppVoiceRecorder.startRecord(messageKey)
+                    } else if (event.action == MotionEvent.ACTION_UP) {
+                        chat_input_message.setText("")
+                        chat_btn_voice.setColorFilter(
+                            ContextCompat.getColor(
+                                APP_ACTIVITY,
+                                R.color.colorBlack
+                            )
+                        )
+                        mAppVoiceRecorder.stopRecord { file, messageKey ->
+                            uploadFileToStorage(
+                                Uri.fromFile(file),
+                                messageKey,
+                                contact.id,
+                                TYPE_MESSAGE_VOICE
+                            )
+                            mSmoothScrollToPosition = true
+                        }
+                    }
+                }
+                true
+            }
+        }
+    }
+
+    private fun attach() {
+        mBottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        btn_attach_file.setOnClickListener { attachFile() }
+        btn_attach_image.setOnClickListener { attachImage() }
+    }
+
+    private fun attachFile() {
+        val intent = Intent(Intent.ACTION_GET_CONTENT)
+        intent.type = "*/*"
+        startActivityForResult(intent, PICK_FILE_REQUEST_CODE)
+    }
+
+    private fun attachImage() {
+        CropImage.activity()
+            .setAspectRatio(1, 1)
+            .setRequestedSize(512, 512)
+            .start(APP_ACTIVITY, this)
+    }
+
+    private fun initRecyclerView() {
+        mRecyclerView = chat_recycler_view
+        mAdapter = GroupChatAdapter()
+        mRefMessages = REF_DATABASE_ROOT.child(NODE_MESSAGES).child(CURRENT_UID).child(contact.id)
+        mRecyclerView.adapter = mAdapter
+        mRecyclerView.setHasFixedSize(true)
+        mRecyclerView.isNestedScrollingEnabled = false
+        mRecyclerView.layoutManager = mLayoutManager
+
+        mMessagesListener = AppChildEventListener {
+            val message = it.getCommonModel()
+
+            if (mSmoothScrollToPosition) {
+                mAdapter.addItemToBottom(AppViewFactory.getView(message)) {
+                    mRecyclerView.smoothScrollToPosition(mAdapter.itemCount)
+                }
+            } else {
+                mAdapter.addItemToTop(AppViewFactory.getView(message)) {
+                    mSwipeRefreshLayout.isRefreshing = false
+                }
+            }
+        }
+
+        mRefMessages.limitToLast(mCountMessages).addChildEventListener(mMessagesListener)
+
+        mRecyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+                if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) mIsScrolling =
+                    true
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (mIsScrolling && dy < 0 && mLayoutManager.findFirstVisibleItemPosition() <= 3) {
+                    updateData()
+                }
+            }
+        })
+
+        mSwipeRefreshLayout.setOnRefreshListener { updateData() }
+    }
+
+    private fun updateData() {
+        mSmoothScrollToPosition = false
+        mIsScrolling = false
+        mCountMessages += 10
+        mRefMessages.removeEventListener(mMessagesListener)
+        mRefMessages.limitToLast(mCountMessages).addChildEventListener(mMessagesListener)
+    }
+
+    private fun initToolbar() {
+        mToolbarInfo = APP_ACTIVITY.mToolbar.toolbar_info
+        mToolbarInfo.visibility = View.VISIBLE
+        mListenerInfoToolbar = AppValueEventListener {
+            mReceivingUser = it.getUserModel()
+            initInfoToolbar()
+        }
+
+        mRefUser = REF_DATABASE_ROOT.child(NODE_USERS).child(contact.id)
+        mRefUser.addValueEventListener(mListenerInfoToolbar)
+        chat_btn_send_message.setOnClickListener {
+            mSmoothScrollToPosition = true
+            val message = chat_input_message.text.toString()
+            if (message.isEmpty()) {
+                showToast("Please input message")
+            } else sendMessage(message, contact.id, TYPE_TEXT) {
+                saveToMainList(contact.id, TYPE_CHAT)
+                chat_input_message.setText("")
+            }
+        }
+    }
+
+    private fun initInfoToolbar() {
+        if (mReceivingUser.fullname.isEmpty()) {
+            mToolbarInfo.toolbar_chat_fullname.text = contact.fullname
+        } else mToolbarInfo.toolbar_chat_fullname.text = mReceivingUser.fullname
+        mToolbarInfo.toolbar_chat_image.downloadAndSetImage(mReceivingUser.photoUrl)
+        mToolbarInfo.toolbar_chat_status.text = mReceivingUser.state
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (data != null) {
+            when (requestCode) {
+                CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE -> {
+                    val uri = CropImage.getActivityResult(data).uri
+                    val messageKey = getMessageKey(contact.id)
+                    uploadFileToStorage(uri, messageKey, contact.id, TYPE_MESSAGE_IMAGE)
+                    mSmoothScrollToPosition = true
+                }
+                PICK_FILE_REQUEST_CODE -> {
+                    val uri = data.data
+                    val messageKey = getMessageKey(contact.id)
+                    val filename = getFilenameFromUri(uri!!)
+                    uploadFileToStorage(uri, messageKey, contact.id, TYPE_MESSAGE_FILE, filename)
+                    mSmoothScrollToPosition = true
+                }
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mToolbarInfo.visibility = View.GONE
+        mRefUser.removeEventListener(mListenerInfoToolbar)
+        mRefMessages.removeEventListener(mMessagesListener)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mAppVoiceRecorder.releaseRecorder()
+        mAdapter.onDestroy()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        activity?.menuInflater?.inflate(R.menu.single_chat_action_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_clear_chat -> clearChat(contact.id){
+                showToast("Chat was cleared")
+                replaceFragment(MainListFragment())
+            }
+            R.id.menu_delete_chat -> deleteChat(contact.id){
+                showToast("Chat was removed")
+                replaceFragment(MainListFragment())
+            }
+        }
+        return true
+    }
+}
